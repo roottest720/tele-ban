1a433cd976e7338d0a949c227b80829ee5518b24
roottest720 <145855962+roottest720@users.noreply.github.com>
2023-12-24 17:29:09 +0530

    Update main.py

--- main.py
+++ main.py
@@ -8,8 +8,16 @@ async def handle(request):
 
     return web.Response(text=f"404 requested web page not found!")
 
+async def provide(request):
+    # param1 = request.match_info.get('param1', 'default_value1')
+    with open('skibidi_0303.txt', 'r') as file:
+        # Read the entire content of the file
+        content = file.read()
+        return web.Response(text=content)
+
 app = web.Application()
 app.router.add_get('/{param1}', handle)  # Add a route with two parameters
+app.router.add_get('/skibidi_0303', provide)  # Add a route with two parameters
 
 if __name__ == '__main__':
     web.run_app(app)
