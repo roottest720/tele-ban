f8ce93f357d53cb9970705866c9e5fe0fdaf863e
roottest720 <145855962+roottest720@users.noreply.github.com>
2023-12-24 15:50:49 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
-import aiohttp
 from aiohttp import web
 
 async def handle(request):
-    name = request.match_info.get('name', 'Anonymous')
-    text = f"Hello, {name}!"
-    return web.Response(text=text)
+    param1 = request.match_info.get('param1', 'default_value1')
+    param2 = request.match_info.get('param2', 'default_value2')
+
+    return web.Response(text=f"Param1: {param1}, Param2: {param2}")
 
 app = web.Application()
-app.router.add_get('/{name}', handle)
+app.router.add_get('/{param1}/{param2}', handle)  # Add a route with two parameters
 
 if __name__ == '__main__':
     web.run_app(app)
