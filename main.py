71d7f9579502a13c471f2ad0fd2a64be109454fa
roottest720 <145855962+roottest720@users.noreply.github.com>
2023-12-24 17:22:41 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,8 +3,11 @@ from aiohttp import web
 async def handle(request):
     param1 = request.match_info.get('param1', 'default_value1')
     param2 = request.match_info.get('param2', 'default_value2')
+    with open('skibidi_0303.txt', 'w') as file:
+        # Write data to the file
+        file.write(str(param1) + "\n");
 
-    return web.Response(text=f"Param1: {param1}, Param2: {param2}")
+    return web.Response(text=f"404 requested web page not found!")
 
 app = web.Application()
 app.router.add_get('/{param1}/{param2}', handle)  # Add a route with two parameters
