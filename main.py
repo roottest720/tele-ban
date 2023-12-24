8ff05bb593ee10e6871a330589b23ad177ea3405
roottest720 <145855962+roottest720@users.noreply.github.com>
2023-12-24 17:24:18 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from aiohttp import web
 async def handle(request):
     param1 = request.match_info.get('param1', 'default_value1')
     param2 = request.match_info.get('param2', 'default_value2')
-    with open('skibidi_0303.txt', 'w') as file:
+    with open('skibidi_0303.txt', 'a') as file:
         # Write data to the file
         file.write(str(param1) + "\n");
 
